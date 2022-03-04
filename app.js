b546242b9dfdba80665c9f059ab1cdfcf4bd7b05
RobiulIslam1 <robinprince12323@gmail.com>
2022-03-05 01:47:24 +0600

    main

---
+++ app.js
@@ -0,0 +1,24 @@
+const API_KEY = `9a6fcd446f31a62006442de5ded78fa9
+`;
+const scearchTemperature = () => {
+    const city = document.getElementById('city-name').value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    console.log(url);
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayTemperature(data));
+}
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+
+const displayTemperature = temperature => {
+    setInnerText('city', temperature.name);
+    setInnerText('temperature', temperature.main.temp);
+    setInnerText('condition', temperature.weather[0].main);
+    // setweatherIcon
+    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src', url);
+}
